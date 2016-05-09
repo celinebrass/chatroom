69c0c39c7cc104907f86a370d299b56fd33f2f6f
Celine Brass <cjb327@cornell.edu>
2016-05-09 19:25:08 -0400

    updated db configs for production

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
-
-	'url' : 'localhost:27017/PongStats' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+	'url' : 'ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/PongStats' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
