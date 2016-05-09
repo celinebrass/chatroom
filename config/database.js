b6781be1ee1dfbbe93a1f6a2c594af9ff3fd6831
Celine Brass <cjb327@cornell.edu>
2016-05-09 19:47:52 -0400

    oijoij

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url' : 'mongodb://root:DyvslnyYX9wGF@ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/PongStats' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://bitnami:DyvslnyYX9wGF@ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/PongStats' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
