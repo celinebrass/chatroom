d00bafa5be1a4f300d7f8ddd01db5d4bc8a7f8b3
Celine Brass <cjb327@cornell.edu>
2016-05-10 02:02:30 -0400

    oijoij

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 var password = process.env.mongoPW
 module.exports = {
-	'url' : 'mongodb://pianoboe105:'+password+'@ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/dummyDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://pianoboe105:'+password+'@ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/dummyDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'localhost:27017/dummyDB'
 };
