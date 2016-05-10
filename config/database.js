c808d45948048ee6105c701e88e0a4e2d6e7a691
Celine Brass <cjb327@cornell.edu>
2016-05-09 23:55:11 -0400

    please work

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+var password = process.env.mongoPW
 module.exports = {
-	//'url' : 'mongodb://bitnami:DyvslnyYX9wGF@ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/PongStats' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://pianoboe105:'+password+'@ec2-54-200-11-149.us-west-2.compute.amazonaws.com:27017/dummyDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
